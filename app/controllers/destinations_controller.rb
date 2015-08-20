4c4795272e053fe913da6ee8da155485a9c197b3
Loren Wang <wang131.131@gmail.com>
2015-08-20 10:24:46 -0700

    delete destination controller/model

--- app/controllers/destinations_controller.rb
+++
@@ -1,6 +0,0 @@
-class DestinationsController < ApplicationController
-
-  # def destination
-  #   Shipment.new(country: "US", state: state, city: city, postal_code: postal_code)
-  # end
-end
--- app/models/destination.rb
+++
@@ -1,7 +0,0 @@
-class Destination < ActiveRecord::Base
-  # validates :street, presence: true
-  validates :country, presence: true
-  validates :city, presence: true
-  validates :state, presence: true
-  validates :zip, presence: true
-end

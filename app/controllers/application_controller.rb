e062697812d659ada48f43fe218ac02e7798a19e
Brenna <brennaleker@gmail.com>
2015-08-19 17:43:29 -0700

    fixed box_size

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,26 +13,40 @@ class ApplicationController < ActionController::Base
     state = @betsy_shipping["state"]
     zip = @betsy_shipping["zip"]
     @destination = ActiveShipping::Location.new(country: country, state: state, city: city, zip: zip)
-    # @destination = ActiveShipping::Location.new(country:"US", state: "MT", city: "Boise", zip: "59001")
   end
 
   def package
-    @package = ActiveShipping::Package.new( @betsy_shipping["total_weight"].to_i,             # 7.5 lbs, times 16 oz/lb.
-                                          [15, 10, 4.5],        # 15x10x4.5 inches
-                                          :units => :imperial)  # not grams, not centimetres
+    @box_size = @betsy_shipping["box_size"]
+    if @box_size == "large"
+      @box_size = [16, 12, 8]
+    elsif @box_size == "medium"
+      @box_size = [12, 12, 12]
+    else
+      @box_size = [8, 8, 8]
+    end
+
+    @package = ActiveShipping::Package.new(
+    @betsy_shipping["total_weight"].to_i,
+    @box_size,
+    :units => :imperial)
   end
 
   def estimate_request
     @betsy_shipping = params
+    binding.pry
     origin
     destination
     package
     ups = ActiveShipping::UPS.new(:login => ENV["ACTIVESHIPPING_UPS_LOGIN"], :password => ENV["ACTIVESHIPPING_UPS_PASSWORD"], :key => ENV["ACTIVESHIPPING_UPS_KEY"])
-
     ups_response = ups.find_rates(@origin, @destination, @package)
-
     ups_rates = ups_response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
 
-    render json: ups_rates.as_json
+    usps = ActiveShipping::USPS.new(:login => ENV["ACTIVESHIPPING_USPS_LOGIN"])
+    usps_response = usps.find_rates(@origin, @destination, @package)
+    usps_rates = usps_response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+    carrier_rates = []
+    carrier_rates.push(ups_rates, usps_rates)
+
+    render json: carrier_rates.as_json
   end
 end

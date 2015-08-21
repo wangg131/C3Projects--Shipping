d228c7b64ae0bb7a42db13e9eda2874e92d2defa
Loren Wang <wang131.131@gmail.com>
2015-08-20 20:28:58 -0700

    vcr setup, moar

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'vcr'
 
 VCR.configure do |config|
+  config.allow_http_connections_when_no_cassette = true
   config.cassette_library_dir = "spec/vcr"
-  config.hook_into :webmock 
+  config.hook_into :webmock
 end

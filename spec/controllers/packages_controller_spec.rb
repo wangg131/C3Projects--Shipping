5b3022c37d6d4ef591e046e74ad4536a6285d5cd
Brenna <brennaleker@gmail.com>
2015-08-21 13:52:08 -0700

    one more line of test coverage

--- spec/controllers/packages_controller_spec.rb
+++ spec/controllers/packages_controller_spec.rb
@@ -32,6 +32,15 @@ RSpec.describe PackagesController, type: :controller do
     end
   end
 
+  describe "changes box size to dimensions" do
+    it "changes the box size received form bEtsy to dimensions that can be comsumed by the shipping API gem" do
+      @controller = PackagesController.new
+      @box_size = "small"
+      @controller.instance_eval{ set_box_size }
+      expect(@controller.instance_eval{ set_box_size }).to eq([8, 8, 8])
+    end
+  end
+
   describe "parses the betsy shipping request" do
     let(:params) {
       { zip: "98101", city: "Seattle", state: "WA", country: "US", box_size: "medium", total_weight: "12.0", controller: "packages", action: "estimate_request"}
@@ -56,6 +65,12 @@ RSpec.describe PackagesController, type: :controller do
     end
   end
 
+#   describe "rescue methods" do
+#         # PackagesController.stub(:estimate_request).and_raise(Exception)
+#     it ""
+#     expect(response.response_code).to eq(400)
+#   end
+# end
   describe "packages#save" do
     let(:params) do
       {
